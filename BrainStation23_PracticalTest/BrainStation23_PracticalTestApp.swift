d48c1c9366ad13f96bd4f11cecc4a990e8d83bac
rouf69nb <official.rouf69nb@gmail.com>
2022-07-06 21:04:27 +0600

    Initial Commit

---
+++ BrainStation23_PracticalTest/BrainStation23_PracticalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainStation23_PracticalTestApp.swift
+//  BrainStation23_PracticalTest
+//
+//  Created by Md. Abdur Rouf on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BrainStation23_PracticalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
